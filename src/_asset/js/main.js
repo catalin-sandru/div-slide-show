7c65edf32150efe92725ac3fb8bef0b255cd1e5a
Catalin Sandru <catalin.sandru88@gmail.com>
2019-01-02 16:08:04 +0000

    got inner html from container

--- src/_asset/js/main.js
+++ src/_asset/js/main.js
@@ -1,26 +1,29 @@
-let current = document.querySelectorAll('.current');
+let current = [];
 const content = document.querySelectorAll('.container');
 const contentArr = Array.from(content);
+current = contentArr[0].innerHTML
 console.log(contentArr[1]);
 
 let prev = document.querySelector('.prev');
 prev.addEventListener('mousedown', prevSlide);
 let next = document.querySelector('.next');
 next.addEventListener('click', nextSlide);
-initSlider()
+initSlider();
 
-// function initSlider() {
-//   current == contentArr.length.innerHTML;
-//   console.log(current);
-// }
+function initSlider() {
+  current = document.querySelector('.current');
+  console.log(current);
+}
 
-// function prevSlide(){
-//   const setDefault = contentArr[0].innerHTML;
+function prevSlide(){
+  // const setDefault = contentArr[0].innerHTML;
+  alert('muie psd');
+  }
+  //   foreach(content) in contentArr
+  //     content.classList.add('current');
+  // }
   
-//   foreach(content) in contentArr
-//     content.classList.add('current');
-// }
-
-// function nextSlide() {
-//   contentArr.length.push(current);
-// }
+  function nextSlide() {
+    // contentArr.length.push(current)
+    alert('muie psd');
+  }
