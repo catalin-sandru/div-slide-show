9d2cf3e084b964b018f2cb8e1b3b76d8fec3cdd7
freebiker2000 <catalin.sandru88@gmail.com>
2019-01-04 15:54:29 +0000

    a bit of refractoring

--- src/_asset/js/main.js
+++ src/_asset/js/main.js
@@ -1,32 +1,27 @@
 const content = document.querySelectorAll('.container');
 const contentArr = Array.from(content);
-
-function initSlider() {
-  let current = contentArr;
-  let print = "";
-  for(var i = 0; i < current.length; i++){
-    print = current[0].innerHTML;
-  }
+let current = function initSlider() {
+  let print = [];
+    print = contentArr[0].innerHTML;
+  console.log(print)
   return print;
 };
 
-document.getElementById('current').innerHTML = initSlider();
-  
-  // function prevSlide(){
-  //   // const setDefault = cont entArr[0].innerHTML;
-  //   alert('muie la ficat');
-  // }
-  // //   foreach(content) in contentArr
-  // //     content.classList.add('current');
-  // // }
+// console.log()
 
-  // next.addEventListener('click', nextSlide);
-  // let prev = document.querySelector('.prev');
-  // prev.addEventListener('mousedown', prevSlide);
-  // let next = document.querySelector('.next');
-  
-  // function nextSlide() {
-  //   // contentArr.length.push(current)
-  //   alert('muie psd');
-  // }
+let next = document.querySelector('.next');
+next.addEventListener('click', nextSlide);
 
+let prev = document.querySelector('.prev');
+prev.addEventListener('mousedown', prevSlide);
+
+function prevSlide(){
+  
+  console.log(current);
+}
+
+function nextSlide() {
+  
+}
+
+document.getElementById('current').innerHTML = current();
